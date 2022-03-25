c26f4c3bd739f2e8ad7099c7832ca642d09fe51f
Jake <asaxplayinghorse@gmail.com>
2022-03-24 17:37:25 -0700

    Added heapsort.

--- Cpp/Sorting/main.cpp
+++ Cpp/Sorting/main.cpp
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <vector>
 #include <iostream>
+#include <chrono>
 #include "quicksort.h"
 #include "mergesort.h"
+#include "heapsort.h"
 using namespace std;
 
 // Sorting - main.cpp
@@ -27,10 +29,14 @@ void printVector(vector<int> v) {
     cout << "}; \n";
 }
 
+using std::chrono::system_clock;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+
 int main(int argc, char ** argv) {
 
     // First, build some test data
-    int arrSize = (int)1e5;
+    int arrSize = (int)1e7;
     vector<int> a;
 
     for (int i=0; i<arrSize; i++) {
@@ -39,19 +45,27 @@ int main(int argc, char ** argv) {
 
     // Call sorting function
     vector<int> asorted;
-    bool doQuickSort = false;
+    bool doQuickSort = true;
+    bool doHeapSort = false;
 
+    auto start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     if (doQuickSort) {
         int * aq;
         aq = a.data();
         quicksort(aq, 0, a.size()-1);
         asorted = a;
+    } else if (doHeapSort) {
+        int * ah = a.data();
+        heapSort(ah, a.size());
+        asorted = a;
     } else {
         asorted = mergeSort(a);
     }
 
+    auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     // Check that our sorting routine has correctly sorted 
-    printf("Testing sort on an array with %d elements via %s.\n", arrSize, doQuickSort?"Quicksort":"Mergesort");
+    printf("Testing sort on an array with %d elements via %s.\n", arrSize, doQuickSort?"Quicksort":doHeapSort?"Heapsort":"Mergesort");
+    printf("Took %6ld ms\n", end-start);
     printf("isSorted: %s\n", isSorted(asorted)? "True":"False");
     return 1;
 }
